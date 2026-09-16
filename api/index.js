2e90dd389c1e308ae996676f1671b0a315db9163
jiya1321 <jiyaagnihotri459@gmail.com>
2026-09-16 22:10:36 +0530

    fix Vercel deployment

---
+++ api/index.js
@@ -0,0 +1,15 @@
+// Import from the built CommonJS bundle
+const serverModule = require("../dist/index.cjs");
+const { initializeApp, app } = serverModule;
+
+let initializationPromise = null;
+
+module.exports = async function handler(req, res) {
+  if (!initializationPromise) {
+    initializationPromise = initializeApp();
+  }
+
+  await initializationPromise;
+
+  return app(req, res);
+};
